b87dc848132052a586b863b303a00dab856ebb98
a.forghan <a.forghani.y79@gmail.com>
2020-01-07 08:19:41 +0330

    base of login page added.

---
+++ src/sample/Controllers/loginPage.java
@@ -0,0 +1,18 @@
+package sample.Controllers;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loginPage implements Initializable {
+
+
+    public AnchorPane paneBackground;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

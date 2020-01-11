fbe572d84c373cea0fc8fa56628402217bb9d92a
a.forghan <a.forghani.y79@gmail.com>
2020-01-11 22:36:30 +0330

    import new students class added with persian comments

--- src/sample/DataCenter/ImportNewStudent.java
+++ src/sample/DataCenter/ImportNewStudent.java
@@ -1,6 +1,8 @@
 package sample.DataCenter;
 
 
+import java.io.*;
+
 // یه کلاس برای اضافه کردن دانشجو های جدید از فایل CSV
 // که از سازمان سنجش دریافت شده است
 //سازنده یا contractor کلاس فقط آدرس فایل CSV و آدرس فایل باینری مقصد رو خواهد گرفت
@@ -8,12 +10,59 @@ package sample.DataCenter;
 public class ImportNewStudent {
 
 
-    private String CSV_addr ;
-    private String Bin_addr ;
+    private String CSV_addr;
+    private String Bin_addr;
+    private NewStudent[] students;
+
+
+
+    FileReader fileReader;
+    BufferedReader bufferedReader;
+    FileOutputStream fileOutputStream;
+    ObjectOutputStream objectOutputStream;
+
+
+    private String status;
+    private int statusCode = 0;
 
     ImportNewStudent(String CSV_address, String Binary_address) {
-    this.CSV_addr = CSV_address;
-    this.Bin_addr = Binary_address;
+        this.Bin_addr = Binary_address;
+        this.CSV_addr = CSV_address;
+        setup();
+
+
+    }
+
+
+//TODO read information from csv-file , make new student object and write in bin file
+
+
+
+    public String getStatus() {
+        return this.status;
+    }
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+
+    void setup() {
+        try {
+            fileReader = new FileReader(CSV_addr);
+            bufferedReader = new BufferedReader(fileReader);
+            fileOutputStream = new FileOutputStream(Bin_addr);
+            objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            status = "successful";
+            statusCode = 0;
+
+
+        } catch (Exception e) {
+            if (statusCode == 0) {
+                statusCode = 1;
+                status += e.getMessage();
+            }
+        }
+
     }
 
 

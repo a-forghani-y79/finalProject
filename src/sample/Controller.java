e68a6a4f4b2d6b5b7e4356c854b071652ea05d28
aliIsazadeh <isazadhali@gmail.com>
2020-01-11 16:17:18 +0330

    Merge branch 'master' of C:\Users\Asus\IdeaProjects\finalProject1 with conflicts.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ResourceBundle;
+
+public class Controller  implements Initializable {
+
+
+    public AnchorPane pane;
+    public  void test(){
+        LocalDateTime myobj = LocalDateTime.now();
+        System.out.println(myobj.getHour());
+
+        //pane.setStyle("-fx-background-image: /sample/FXML/PNG/1pic.png");
+        pane.setStyle("-fx-background-image: url(../sample/PNG/1pic.png)");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    //test();
+
+    }
 }

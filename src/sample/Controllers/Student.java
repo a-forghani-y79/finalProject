3b30a78a157646d22081187dd4395c8f0b1cab98
tic tac toe <shakryhsyn1@gmail.com>
2020-01-19 22:38:54 +0330

    Add tabPane to Student Class

---
+++ src/sample/Controllers/Student.java
@@ -0,0 +1,16 @@
+package sample.Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Student implements Initializable {
+    sample.DataCenter.Student student;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

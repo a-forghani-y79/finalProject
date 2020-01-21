ca973b446a83b769b5759b83b49eb17da3979602
tic tac toe <shakryhsyn1@gmail.com>
2020-01-21 14:50:51 +0330

    طراحی دیزاین نشان دادن اطلاعات دانشجو

--- src/sample/Controllers/Student.java
+++ src/sample/Controllers/Student.java
@@ -1,6 +1,9 @@
 package sample.Controllers;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 
 import java.net.URL;
@@ -8,11 +11,24 @@ import java.util.ResourceBundle;
 
 public class Student implements Initializable {
     sample.DataCenter.Student student;
-
+        public TableView tableView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Image img = new Image("./sample/PNG/logoUni.png");
+
+        TableColumn<String, Person> column1 = new TableColumn<>("First Name");
+        column1.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+
+
+        TableColumn<String, Person> column2 = new TableColumn<>("Last Name");
+        column2.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+
+
+        tableView.getColumns().add(column1);
+        tableView.getColumns().add(column2);
+
+        tableView.getItems().add(new Person("John", "Doe"));
+        tableView.getItems().add(new Person("Jane", "Deer"));
 
     }
 }
---
+++ src/sample/DataCenter/personMethod.java
@@ -0,0 +1,33 @@
+package sample.DataCenter;
+
+public class personMethod {
+
+    private String firstName = null;
+    private String lastName = null;
+
+    public personMethod() {
+    }
+
+    public personMethod(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+}

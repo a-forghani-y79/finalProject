8e423970505dc937af72e459bf2c1adf2390720b
aliIsazadeh <isazadhali@gmail.com>
2020-01-23 18:13:26 +0330

    Merge branch 'master' of C:\Users\Asus\IdeaProjects\finalProject1 with conflicts.

--- src/sample/DataCenter/Student.java
+++ src/sample/DataCenter/Student.java
@@ -8,33 +8,33 @@ public class Student {
     private long studentNumber;
     private Field[] fields;
 
-    long rate;
-    String field;
-    boolean isDay; //true for roozaneh
-    long fileNumber;
-    long PhoneNumber;
-    Long FatherPhoneNumber;
-    String FatherName;
-    String Faculty;
-    long IDCardNumber;
-    String Address;
-    String SectionEducation;
-    String Condition;
-    String StartSeason;
-    String Nationality;
-    String Course;
-    String DiplomType;
-    String Matrimony;
-    String LocalBorn;
-    int DiplomYear;
-    String UBorn;
-    String Faith;
-    String Religion;
-    String CityBorn;
-    String Email;
-    long ZIPCode;
-    String AreaBorn;
-    String FileNumber1;
+    private long rate;
+    private String field;
+    private boolean isDay; //true for roozaneh
+    private long fileNumber;
+    private long PhoneNumber;
+    private Long FatherPhoneNumber;
+    private String FatherName;
+    private String Faculty;
+    private long IDCardNumber;
+    private String Address;
+    private String SectionEducation;
+    private  String Condition;
+    private  String StartSeason;
+    private  String Nationality;
+    private String Course;
+    private String DiplomType;
+    private String Matrimony;
+    private String LocalBorn;
+    private int DiplomYear;
+    private String UBorn;
+    private String Faith;
+    private String Religion;
+    private String CityBorn;
+    private String Email;
+    private long ZIPCode;
+    private String AreaBorn;
+    private String FileNumber1;
 
 
 }

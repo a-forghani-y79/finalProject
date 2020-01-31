dff729cc2e85f62fc9acc67366e73ee707bc833a
a.forghan <a.forghani.y79@gmail.com>
2020-02-01 01:02:33 +0330

    RAndD department in TestTools added. Navigation drawer menu added. ReadMe was written.

---
+++ src/sample/TestTools/RAndD/Navigation_Drawer_Menu/Main.java
@@ -0,0 +1,30 @@
+package sample.TestTools.RAndD.Navigation_Drawer_Menu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        Parent root;
+        loader.setLocation(getClass().getResource("/sample/TestTools/RAndD/Navigation_Drawer_Menu/main.fxml"));
+        root = loader.load();
+        primaryStage.setScene(new Scene(root, 1058, 600));
+        primaryStage.show();
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
